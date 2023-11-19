7b0ba9f4b1e12bdc9e91c5ae250f2a61bd140200
Thomas Eykamp <embergameslp@gmail.com>
2023-11-19 02:19:44 -0500

    clean up backend

--- server/index.ts
+++ server/index.ts
@@ -6,28 +6,12 @@ const app = express();
 app.use(express.json());
 dotenv.config();
 
-const conString = process.env.DB_CONNECTION_URL;
-const client = new pg.Client(conString);
-
 const queriesRoute = require('./queries');
 app.use('/api', queriesRoute);
 
 app.get('/api', (req, res) => {
   res.json({
     message: 'Hello World!'
-  });
-});
-
-client.connect((err) => {
-  if (err) {
-    return console.error('could not connect to postgres', err);
-  }
-  client.query('SELECT * FROM project', (err, result) => {
-    if (err) {
-      return console.error('error running query', err);
-    }
-    console.log(result.rows);
-    client.end();
   });
 });
 
