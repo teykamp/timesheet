f794ed0f99bc67d74a0484a670d99dc9aa849612
Thomas Eykamp <embergameslp@gmail.com>
2023-11-18 23:19:12 -0500

    add database connection

--- server/index.ts
+++ server/index.ts
@@ -6,6 +6,23 @@ const app = express();
 app.use(express.json());
 dotenv.config();
 
+var conString = process.env.DB_CONNECTION_URL
+var client = new pg.Client(conString);
+
+client.connect((err) => {
+  if(err) {
+    return console.error('could not connect to postgres', err);
+  }
+  client.query('SELECT * FROM project', function(err, result) {
+    if(err) {
+      return console.error('error running query', err);
+    }
+    console.log(result);
+    client.end();
+  });
+});
+
+
 app.listen(3000, () => {
   console.log('Server is listening on port 2000. Ready to accept requests!');
 });
