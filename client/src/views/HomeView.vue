1f8a550047fb414dcfe24524e6097892776d80dc
yonava <yona@vossandreae.com>
2023-11-18 23:02:15 -0500

    tailwind and vuetify up and running

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+const msg = (aMsg: string) => {
+  alert(aMsg)
+}
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <v-btn>
+      hello
+    </v-btn>
   </main>
 </template>
