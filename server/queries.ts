6cd7c83503fb428941dcdc795b8123bc967a7ca0
Thomas Eykamp <embergameslp@gmail.com>
2023-11-19 03:27:56 -0500

    add users queries

--- server/queries.ts
+++ server/queries.ts
@@ -317,14 +317,173 @@ router.put('/timesheetEntries/:entryID', async (req, res) => {
   }
 });
 
-// post a user
-// get a user
-// get all users
-// delete a user
-// delete all users
-// get all managers
-// get all users of a manager
-// update user's manager
-// update user's manager status
+/*
+  USERS
+*/
+
+// POST a user
+router.post('/users', async (req, res) => {
+  const { username, email, managerID, isManager } = req.body;
+
+  try {
+    await client.connect();
+    const result = await client.query(
+      'INSERT INTO "user" (username, email, managerID, isManager) VALUES ($1, $2, $3, $4) RETURNING *',
+      [username, email, managerID, isManager]
+    );
+    res.status(201).json(result.rows[0]);
+  } catch (error) {
+    res.status(500).json({ error: 'Error inserting user into the database' });
+  } finally {
+    await client.end();
+  }
+});
+
+// GET a user by userID
+router.get('/users/:userID', async (req, res) => {
+  const { userID } = req.params;
+
+  try {
+    await client.connect();
+    const result = await client.query('SELECT * FROM "user" WHERE userID = $1', [userID]);
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    res.json(result.rows[0]);
+  } catch (error) {
+    res.status(500).json({ error: 'Error retrieving user from the database' });
+  } finally {
+    await client.end();
+  }
+});
+
+// GET all users
+router.get('/users', async (req, res) => {
+  try {
+    await client.connect();
+    const result = await client.query('SELECT * FROM "user"');
+    res.json(result.rows);
+  } catch (error) {
+    res.status(500).json({ error: 'Error retrieving users from the database' });
+  } finally {
+    await client.end();
+  }
+});
+
+// DELETE a user by userID
+router.delete('/users/:userID', async (req, res) => {
+  const { userID } = req.params;
+
+  try {
+    await client.connect();
+    const result = await client.query('DELETE FROM "user" WHERE userID = $1 RETURNING *', [userID]);
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    res.json(result.rows[0]);
+  } catch (error) {
+    res.status(500).json({ error: 'Error deleting user from the database' });
+  } finally {
+    await client.end();
+  }
+});
+
+// DELETE all users
+router.delete('/users', async (req, res) => {
+  try {
+    await client.connect();
+    const result = await client.query('DELETE FROM "user" RETURNING *');
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: 'No users found' });
+    }
+
+    res.json(result.rows);
+  } catch (error) {
+    res.status(500).json({ error: 'Error deleting users from the database' });
+  } finally {
+    await client.end();
+  }
+});
+
+// GET all managers
+router.get('/managers', async (req, res) => {
+  try {
+    await client.connect();
+    const result = await client.query('SELECT * FROM "user" WHERE isManager = true');
+    res.json(result.rows);
+  } catch (error) {
+    res.status(500).json({ error: 'Error retrieving managers from the database' });
+  } finally {
+    await client.end();
+  }
+});
+
+// GET all users of a manager
+router.get('/users/manager/:managerID', async (req, res) => {
+  const { managerID } = req.params;
+
+  try {
+    await client.connect();
+    const result = await client.query('SELECT * FROM "user" WHERE managerID = $1', [managerID]);
+    res.json(result.rows);
+  } catch (error) {
+    res.status(500).json({ error: 'Error retrieving users of the manager from the database' });
+  } finally {
+    await client.end();
+  }
+});
+
+// UPDATE user's manager
+router.put('/users/:userID/manager', async (req, res) => {
+  const { userID } = req.params;
+  const { managerID } = req.body;
+
+  try {
+    await client.connect();
+    const result = await client.query(
+      'UPDATE "user" SET managerID = $1 WHERE userID = $2 RETURNING *',
+      [managerID, userID]
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    res.json(result.rows[0]);
+  } catch (error) {
+    res.status(500).json({ error: 'Error updating user\'s manager in the database' });
+  } finally {
+    await client.end();
+  }
+});
+
+// UPDATE user's manager status
+router.put('/users/:userID/manager/status', async (req, res) => {
+  const { userID } = req.params;
+  const { isManager } = req.body;
+
+  try {
+    await client.connect();
+    const result = await client.query(
+      'UPDATE "user" SET isManager = $1 WHERE userID = $2 RETURNING *',
+      [isManager, userID]
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    res.json(result.rows[0]);
+  } catch (error) {
+    res.status(500).json({ error: 'Error updating user\'s manager status in the database' });
+  } finally {
+    await client.end();
+  }
+});
 
 export = router
