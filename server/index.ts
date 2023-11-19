dd3c7434fe9faf1dc91a04fab584123b3a170118
yonava <yona@vossandreae.com>
2023-11-18 21:55:56 -0500

    ts-node and nodemon work

--- server/index.ts
+++ server/index.ts
@@ -2,11 +2,10 @@ import pg from 'pg';
 import dotenv from 'dotenv';
 import express from 'express';
 
-
 const app = express();
 app.use(express.json());
 dotenv.config();
 
 app.listen(3000, () => {
-  console.log('Server is listening on port 3000. Ready to accept requests!');
+  console.log('Server is listening on port 2000. Ready to accept requests!');
 });
