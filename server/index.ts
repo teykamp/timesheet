72cec5b72bcbbce3c17b3ea9e8f78f4bccc2b66f
Thomas Eykamp <embergameslp@gmail.com>
2023-11-26 15:17:07 -0500

    add basic google auth barebones

--- server/index.ts
+++ server/index.ts
@@ -1,13 +1,110 @@
 import dotenv from 'dotenv';
 import express from 'express';
 
+const { google } = require('googleapis');
+
 const app = express();
 app.use(express.json());
 dotenv.config();
 
+const { OAuth2 } = google.auth;
+const { GOOGLE_OAUTH_CLIENT_ID, GOOGLE_OAUTH_CLIENT_SECRET } = process.env;
+const redirectUri = process.env.NODE_ENV ? 'https://colab.up.railway.app/auth' : 'http://localhost:5177/auth';
+module.exports.redirectUri = redirectUri;
+
+const scope = [
+  'https://www.googleapis.com/auth/userinfo.profile',
+  'https://www.googleapis.com/auth/userinfo.email',
+]
+
+const getGoogleProfileData = async (accessToken) => {
+  try {
+    const auth = new OAuth2(
+      GOOGLE_OAUTH_CLIENT_ID,
+      GOOGLE_OAUTH_CLIENT_SECRET,
+      redirectUri
+    );
+    auth.setCredentials({ access_token: accessToken });
+    const oauth2 = google.oauth2({ auth, version: 'v2' });
+    const { data } = await oauth2.userinfo.get();
+    return data;
+  } catch (e) {
+    // console.log(e)
+    throw "Invalid Grant: Google Profile Data";
+  }
+}
+
+function getAuthUrl() {
+  const auth = new OAuth2(
+    GOOGLE_OAUTH_CLIENT_ID,
+    GOOGLE_OAUTH_CLIENT_SECRET,
+    redirectUri
+  );
+
+  return auth.generateAuthUrl({
+    access_type: 'offline',
+    login_hint: 'select_account',
+    scope,
+  });
+}
+
 const queriesRoute = require('./queries');
 
 app.use('/api', queriesRoute);
+
+app.get('/api/auth/url', (req, res) => {
+  res.json({ url: getAuthUrl() });
+}); // redirect to ths url to login
+
+app.get('/api/auth/:authCode', async (req, res) => {
+  const { authCode } = req.params;
+  try {
+    const auth = new OAuth2(
+      GOOGLE_OAUTH_CLIENT_ID,
+      GOOGLE_OAUTH_CLIENT_SECRET,
+      redirectUri
+    );
+    const { tokens } = await auth.getToken(authCode);
+    const { access_token: accessToken } = tokens;
+
+    // respond with some shit, probabily the access token
+  } catch (e) {
+    // this tells the caller that their code is a load of bullshit and to fuck their mo with it
+    res.json({
+      error: 'INVALID AUTH CODE'
+    });
+  }
+})
+
+app.get('/api/user', async (req, res) => {
+  const { authorization } = req.headers;
+  const accessToken = authorization.split(' ')[1];
+
+  try {
+    const userProfile = await getGoogleProfileData(accessToken);
+    const userEmail = await 1 + 1 // actual call here
+    res.json({
+      profileData: {
+        ...userProfile,
+        userEmail
+      }
+    });
+  } catch (e) {
+    console.log(e)
+    res.status(401).json({ error: 'Forbidden' });
+  }
+});
+
+// export type GoogleProfile = {
+//   id: string,
+//   name: string,
+//   given_name: string,
+//   family_name: string,
+//   picture: string,
+//   locale: string
+// }
+
+// DB API
 
 app.get('/api', (req, res) => {
   res.json({
