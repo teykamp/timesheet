09c345bcb1d4bcf6d2e5f1326e01a56fc7124d2f
Thomas Eykamp <embergameslp@gmail.com>
2023-11-30 22:34:27 -0500

    start of  auth get profile

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,21 +1,49 @@
 <template>
   <div>
-    <h1>Google Authentication</h1>
     <button @click="redirectToGoogleAuth">Authenticate with Google</button>
+    <div v-if="userProfile">
+      <p>User Profile:</p>
+      <pre>{{ userProfile }}</pre>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import axios from 'axios'
+import axios from 'axios';
+import { ref, onMounted } from 'vue';
 
-const redirectToGoogleAuth = () => {
-  axios.get('/api/auth/url')
-    .then(response => {
-      const authUrl = response.data.url
-      window.open(authUrl, '_blank')
-    })
-    .catch(error => {
-      console.error('Error fetching authentication URL', error.response.data)
-    })
-}
+const userProfile = ref(null);
+
+const redirectToGoogleAuth = async () => {
+  try {
+    const response = await axios.get('api/auth/url');
+    const authUrl = response.data.url;
+    window.location.href = authUrl;
+  } catch (error) {
+    console.error('Error fetching authentication URL', error.response.data);
+  }
+};
+
+onMounted(async () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const authCode = urlParams.get('code')
+
+  if (authCode) {
+    try {
+      const response = await axios.get(`api/auth/${authCode}`);
+      const accessToken = response.data.access_token;
+
+      try {
+        const profileResponse = await axios.get('api/user', {
+          headers: { Authorization: `Bearer ${accessToken}` }
+        });
+        userProfile.value = profileResponse.data.profileData;
+      } catch (error) {
+        console.error('Error fetching user profile data', error.response.data);
+      }
+    } catch (error) {
+      console.error('Error exchanging authorization code for access token', error.response.data);
+    }
+  }
+});
 </script>
