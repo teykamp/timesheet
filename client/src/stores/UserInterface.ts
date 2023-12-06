1a4e23c725c030687abb7746374d805920f46e1a
Thomas Eykamp <embergameslp@gmail.com>
2023-12-05 19:13:31 -0500

    update UIstore

--- client/src/stores/UserInterface.ts
+++ client/src/stores/UserInterface.ts
@@ -1,11 +1,22 @@
 import { defineStore } from 'pinia'
 
-export const useSnackbar = defineStore('snackbar', {
-  state: () => ({
-    colorName: 'color',
-  }),
-  actions: {
-
-  }
+export const useColorPalette = defineStore('colors',{
+	state: () => ({
+		white: '#ffffff',
+		black: '#000000',
+		gray: '#dedede',
+		blue: '#3b97fc',
+		blueShadow: '#0f63ca',
+		green: '#24c834',
+		greenShadow: '#1d9e2b',
+		greenInner: '#9df2a7',
+		greenOuter: '#1d9e2b',
+		red: '#ff2e2e',
+		redInner: '#ff8888',
+		redOuter: '#bb0000'
+	}),
+    
+	actions: {
+	}		
 })
 
