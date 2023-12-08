b3863f422189c703d38c8deeac4f01b959c2a66a
Thomas Eykamp <embergameslp@gmail.com>
2023-12-07 23:27:10 -0500

    responsive mobile nav

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,92 +1,54 @@
 <template>
   <div>
-    <v-card
-      elevation="1"
-      class="h-16 rounded-0 d-flex align-center"
-    >
-      <v-avatar v-if="smAndUp"
-        class="mx-10"
-        size="x-large"
-        color="info"
-        icon="mdi-clock-outline"
-      >
-      </v-avatar>
-      <v-btn
-        v-if="xs"
-        @click="showDrawer = !showDrawer"
-        class="ml-4"
-        flat
-        icon="mdi-menu">
+    <v-card elevation="1" :class="`h-16 rounded-0 d-flex ${xs ? 'justify-space-between' : ''} align-center`">
+      <v-avatar v-if="mdAndUp" class="mx-10" size="x-large" color="info" icon="mdi-clock-outline"></v-avatar>
+      <v-btn v-if="smAndDown" @click="showDrawer = !showDrawer" class="mx-4" flat icon="mdi-menu">
       </v-btn>
-      
+
       <Teleport to="body">
-        <v-navigation-drawer
-          v-model="showDrawer"
-          location="bottom"
-          temporary
-        >
-          <v-list-item title="My Application" subtitle="Vuetify"></v-list-item>
+        <v-navigation-drawer v-model="showDrawer" location="bottom" temporary class="h-75">
+          <!-- bell icon only xs -->
+          <v-list-item class="d-flex justify-center mt-6">
+            <v-avatar size="x-large" color="info" icon="mdi-clock-outline"></v-avatar>
+          </v-list-item>
           <v-divider></v-divider>
-          <v-list-item link title="List Item 1"></v-list-item>
-          <v-list-item link title="List Item 2"></v-list-item>
-          <v-list-item link title="List Item 3"></v-list-item>
+          <v-list-item v-for="localRoute in router.getRoutes()" :key="localRoute.path" @click="navLinkClick(localRoute)"
+            flat class="my-2 mx-6 rounded-lg" :style="{
+              background: localRoute.path === currentRoute ? gray : '',
+              color: localRoute.path === currentRoute ? textSelected : textPrimary,
+            }">
+            {{ String(localRoute.name).replace(/^\w/, (c) => c.toUpperCase()) }}
+          </v-list-item>
+          <v-divider></v-divider>
+
         </v-navigation-drawer>
       </Teleport>
 
-      <div 
-        v-if="mdAndUp"
-      >
-        <v-btn 
-          v-for="localRoute in router.getRoutes()"
-          :key="localRoute.path"
-          @click="navLinkClick(localRoute)"
-          flat
-          class="ma-2"
-          :style="{
+      <div v-if="mdAndUp">
+        <v-btn v-for="localRoute in router.getRoutes()" :key="localRoute.path" @click="navLinkClick(localRoute)" flat
+          class="ma-2" :style="{
             background: localRoute.path === currentRoute ? gray : '',
-            color: localRoute.path === currentRoute ? textSelected : textPrimary, 
-          }"
-        >
+            color: localRoute.path === currentRoute ? textSelected : textPrimary,
+          }">
           {{ localRoute.name }}
         </v-btn>
       </div>
-      <v-spacer>
+      <v-spacer v-if="smAndUp">
       </v-spacer>
-      <v-btn
-        class="mx-4"
-        :style="{ 
-          background: blue, 
-          color: white 
-        }"
-        prepend-icon="mdi-plus"
-      >New</v-btn>
-      <v-btn 
-        size="small" 
-        flat 
-        icon="mdi-bell-outline"
-        :style="{
-          color: textPrimary,
-        }"
-      ></v-btn>
+      <v-btn class="mx-5" :style="{
+        background: blue,
+        color: white,
+      }" prepend-icon="mdi-plus">New</v-btn>
+      <v-btn v-if="smAndUp" size="small" flat icon="mdi-bell-outline" :style="{
+        color: textPrimary,
+      }"></v-btn>
 
-      <v-menu
-        v-model="showProfileMenu"
-        :close-on-content-click="false"
-        location="end"
-      >
-      <!-- this should create a dialog from the bottom on mobile -->
+      <v-menu v-model="showProfileMenu" :close-on-content-click="false" location="end">
+        <!-- this should create a dialog from the bottom on mobile -->
         <template v-slot:activator="{ props }">
-          <v-btn
-            icon
-            flat 
-            v-bind="props"
-            class="mx-4 mr-8"
-          >
+          <v-btn icon flat v-bind="props" :class="`ml-4 ${smAndUp ? 'mr-10' : ''}`">
             <v-avatar>
-              <v-img
-                src="https://cdn.vuetifyjs.com/images/john.jpg"
-                alt=""
-              ></v-img>
+              <v-img src="https://cdn.vuetifyjs.com/images/john.jpg" alt=""></v-img>
             </v-avatar>
           </v-btn>
         </template>
@@ -94,16 +56,10 @@
         <!-- menuBox -->
         <v-card min-width="300">
           <v-list>
-            <v-list-item
-              prepend-avatar="https://cdn.vuetifyjs.com/images/john.jpg"
-              title="John Leider"
-              subtitle="Founder of Vuetify"
-            >
+            <v-list-item prepend-avatar="https://cdn.vuetifyjs.com/images/john.jpg" title="John Leider"
+              subtitle="Founder of Vuetify">
               <template v-slot:append>
-                <v-btn
-                  variant="text"
-                  icon="mdi-heart"
-                ></v-btn>
+                <v-btn variant="text" icon="mdi-heart"></v-btn>
               </template>
             </v-list-item>
           </v-list>
@@ -112,36 +68,21 @@
 
           <v-list>
             <v-list-item>
-              <v-switch
-                color="purple"
-                label="Enable messages"
-                hide-details
-              ></v-switch>
+              <v-switch color="purple" label="Enable messages" hide-details></v-switch>
             </v-list-item>
 
             <v-list-item>
-              <v-switch
-                color="purple"
-                label="Enable hints"
-                hide-details
-              ></v-switch>
+              <v-switch color="purple" label="Enable hints" hide-details></v-switch>
             </v-list-item>
           </v-list>
 
           <v-card-actions>
             <v-spacer></v-spacer>
 
-            <v-btn
-              variant="text"
-              @click="showProfileMenu = false"
-            >
+            <v-btn variant="text" @click="showProfileMenu = false">
               Cancel
             </v-btn>
-            <v-btn
-              color="primary"
-              variant="text"
-              @click="showProfileMenu = false"
-            >
+            <v-btn color="primary" variant="text" @click="showProfileMenu = false">
               Save
             </v-btn>
           </v-card-actions>
