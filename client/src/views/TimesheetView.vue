80155faed1f99c1a4b0fa5a37f46ff1667b46af7
Thomas Eykamp <embergameslp@gmail.com>
2023-12-11 22:03:06 -0500

    add vertical scrolling

--- client/src/views/TimesheetView.vue
+++ client/src/views/TimesheetView.vue
@@ -1,88 +1,95 @@
 <template>
-  <v-card 
-    flat
-    :style="{
-      'min-width': '600px',
-      'white-space': 'nowrap',
-      'overflow-x': 'scroll', // need horiz scroll
-  }">
-    <v-card
-      class="d-flex justify-center rounded-0 pr-8 pl-1"
-      elevation="2"
-    >
-      <v-col
-        v-for="(label, index) in colLabels"
-        :key="index"
-        :style="{
-          'min-width': index === 0 ? '200px' : '50px',
-          'max-width': index === 0 ? '500px' : '150px'
-        }"
-      >
-          <div 
-            v-if="lgAndUp"
-            class="text-truncate"
-            >{{ label.lg }}</div>
-          <div
-            v-else
-            class="text-truncate"
-          >{{ label.sm || label.lg }}</div>
-          <!-- <div v-else>{{ label.xs || label.sm || label.lg }}</div> -->
-      </v-col>
-    </v-card>
-    <v-sheet :style="{
-      'max-height': 'calc(95vh - 170px)',
-      overflow: 'auto',
+  <div>
+    <div :style="{
+      'overflow-x': 'auto',
     }">
-      <v-sheet 
-        v-for="(row, rowIndex) in grid" 
-        :key="rowIndex"
-        class="d-flex justify-center"
-      >
-        <v-col 
-          v-for="(cell, colIndex) in row" 
-          :key="colIndex" 
+    <v-card 
+      flat
+      :style="{
+        'min-width': '600px',
+    }">
+        <v-card
+          class="d-flex justify-center rounded-0 pr-8 pl-1"
+          elevation="2"
+        >
+          <v-col
+            v-for="(label, index) in colLabels"
+            :key="index"
+            :style="{
+              'min-width': index === 0 ? '200px' : '50px',
+              'max-width': index === 0 ? '500px' : '150px'
+            }"
+          >
+              <div 
+                v-if="lgAndUp"
+                class="text-truncate"
+                >{{ label.lg }}</div>
+              <div
+                v-else
+                class="text-truncate"
+              >{{ label.sm || label.lg }}</div>
+              <!-- <div v-else>{{ label.xs || label.sm || label.lg }}</div> -->
+          </v-col>
+        </v-card>
+        <v-sheet 
+          class="mt-6"
           :style="{
-            'min-width': colIndex === 0 ? '200px' : '50px',
-            'max-width': colIndex === 0 ? '500px' : '150px'
+            'max-height': 'calc(95vh - 170px)',
+            overflow: 'auto',
           }"
         >
-          <v-autocomplete
-            v-if="colIndex === 0"
-            v-model="cell.text"
-            label="Project Name"
-            :items="projectList"
-            variant="outlined"
-            density="compact"
+          <v-sheet 
+            v-for="(row, rowIndex) in grid" 
+            :key="rowIndex"
+            class="d-flex justify-center"
           >
-            <template v-slot:item="{ props, item }">
-              <v-list-item
-                v-bind="props"
-                :disabled="selectedProjects.includes(item.title)"
-              ></v-list-item>
-            </template>
-          </v-autocomplete>
-          <v-text-field
-            v-if="colIndex !== 0"
-            v-model="cell.text"  
-            variant="outlined" 
-            label="Hours" 
-            density="compact" 
-          />
-        </v-col>
-        <v-btn 
-          size="small"
-          variant="tonal"
-          color="red" 
-          icon="mdi-delete"
-          class="mt-3 mx-2"
-        ></v-btn>
-      </v-sheet>
-    </v-sheet>
-    <v-btn
-      class="ml-10 mb-10"
-      prepend-icon="mdi-plus"
-    >Add</v-btn>
-  </v-card>
+            <v-col 
+              v-for="(cell, colIndex) in row" 
+              :key="colIndex" 
+              :style="{
+                'min-width': colIndex === 0 ? '200px' : '50px',
+                'max-width': colIndex === 0 ? '500px' : '150px'
+              }"
+            >
+              <v-autocomplete
+                v-if="colIndex === 0"
+                v-model="cell.text"
+                label="Project Name"
+                :items="projectList"
+                variant="outlined"
+                density="compact"
+              >
+                <template v-slot:item="{ props, item }">
+                  <v-list-item
+                    v-bind="props"
+                    :disabled="selectedProjects.includes(item.title)"
+                  ></v-list-item>
+                </template>
+              </v-autocomplete>
+              <v-text-field
+                v-if="colIndex !== 0"
+                v-model="cell.text"  
+                variant="outlined" 
+                label="Hours" 
+                density="compact" 
+              />
+            </v-col>
+            <v-btn 
+              size="small"
+              variant="tonal"
+              color="red" 
+              icon="mdi-delete"
+              class="mt-3 mx-2"
+            ></v-btn>
+          </v-sheet>
+        </v-sheet>
+      </v-card>
+    </div>
+  <v-btn
+  class="ml-10 mb-10"
+  prepend-icon="mdi-plus"
+  >Add</v-btn>
+</div>
 </template>
 
 <script setup lang="ts">
@@ -90,7 +97,7 @@ import { ref, computed } from 'vue'
 import { useDisplay } from 'vuetify'
 
 const { lgAndUp } = useDisplay()
-const rows = 10
+const rows = 3
 const cols = 6
 
 // need function here to get dates (library probably)
