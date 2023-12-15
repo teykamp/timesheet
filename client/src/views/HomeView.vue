27df19a1400fe9ced2d7174cbb7665f288d080d0
shotgunfacelift <shotgunfacelift762@protonmail.com>
2023-12-15 12:10:29 -0500

    created homepage

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,11 +1,87 @@
+<style>
+  * {box-sizing: border-box;}
+
+body {
+  font-family: Arial;
+  padding: 10px;
+  background: #ffffff;
+}
+
+.mainBody {   
+  float: left;
+  width: 80%;
+}
+
+.rightSide {
+  float: left;
+  width: 20%;
+  background-color: #ffffff;
+  padding-left: 20px;
+}
+
+.placeholder {
+  background-color: #dedede;
+  width: 100%;
+  padding: 25px;
+  text-align: center;
+}
+
+.floater {
+  background-color: white;
+  padding: 20px;
+}
+
+.row::after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+.footer {
+  padding: 20px;
+  text-align: left;
+  background: #ffffff;
+}
+
+hr {
+  height: 2px;
+  border: none;
+  background-color: #dedede;
+  margin-top: 0px
+}
+</style>
+
 <template>
-  <div>
-    <button @click="redirectToGoogleAuth">Authenticate with Google</button>
+  
+
+  <div class="row">
+
+<div class="mainBody">
+  <div class="floater">
+    <div>
+    <button @click="redirectToGoogleAuth"><b>Click Here to Sign In</b></button>
     <div v-if="userProfile">
       <p>User Profile:</p>
       <pre>{{ userProfile }}</pre>
     </div>
   </div>
+    <div class="placeholder" style="height:300px;">PLACEHOLDER IMAGE</div>
+    <p>Timesheet is a versatile application for management of stuff and things</p>
+    </div>
+</div>
+
+<div class="rightSide">
+  <div class="floater">
+    <div class="placeholder" style="height:400px; margin-top: 0px;">PLACEHOLDER IMAGE</div>
+  </div>
+</div>
+
+</div>
+
+<div class="footer">
+<hr>
+<p>eykamp and shotgunfacelift on GitHub</p>
+</div>
 </template>
 
 <script setup lang="ts">
