4cbe6f3b4dff0007e7f33a383137a5f774d3dba9
Thomas Eykamp <embergameslp@gmail.com>
2023-12-27 20:56:11 -0500

    added login wrapper

---
+++ client/src/components/IsUserLoggedInWrapper.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <slot 
+      v-if="isUserLoggedIn()"
+      name="contentIfLoggedIn"
+    >
+
+    </slot>
+    <slot 
+      v-else
+      name="contentIfNotLoggedIn"
+    >
+      <div 
+        :style="{
+          width: '100%',
+          height: '500px',
+        }"  
+        class="d-flex justify-center align-center"
+      >
+        <div class="text-center">
+          <p class="ma-10">You are not Logged In</p>
+          <v-btn
+            @click="redirectToGoogleAuth()"
+            flat
+          >Log In</v-btn>
+        </div>
+      </div>
+    </slot>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useGoogleUserData } from '../stores/useDataStore'
+
+const { isUserLoggedIn, redirectToGoogleAuth } = useGoogleUserData()
+
+
+</script>
--- client/src/views/TimesheetView.vue
+++ client/src/views/TimesheetView.vue
@@ -1,50 +1,36 @@
 <template>
   <div>
-    <div v-if="isUserLoggedIn()">
-      <div v-if="state === 'allTimesheets'">
-        <TimesheetListDisplay
+    <IsUserLoggedInWrapper>
+      <template #contentIfLoggedIn>
+        <div v-if="state === 'allTimesheets'">
+          <TimesheetListDisplay
+            :updateState="updateState"
+          />
+          <v-container 
+            v-if="!isTimesheetListLoading"
+            class="d-flex justify-end"
+          >
+            <v-btn
+              @click="handleAddNewTimesheet()"
+              prepend-icon="mdi-plus"
+            >
+              New
+            </v-btn>
+          </v-container>
+        </div>
+        <v-btn
+          v-if="state === 'editTimesheet'"
+          @click="updateState('allTimesheets')"
+          icon="mdi-chevron-left"
+          flat
+          class="position-absolute ml-4 mt-2"
+        ></v-btn>
+        <EditTimesheet
+          v-if="state === 'editTimesheet'"
           :updateState="updateState"
         />
-        <v-container 
-          v-if="!isTimesheetListLoading"
-          class="d-flex justify-end"
-        >
-          <v-btn
-            @click="handleAddNewTimesheet()"
-            prepend-icon="mdi-plus"
-          >
-            New
-          </v-btn>
-        </v-container>
-      </div>
-      <v-btn
-        v-if="state === 'editTimesheet'"
-        @click="updateState('allTimesheets')"
-        icon="mdi-chevron-left"
-        flat
-        class="position-absolute ml-4 mt-2"
-      ></v-btn>
-      <EditTimesheet
-        v-if="state === 'editTimesheet'"
-        :updateState="updateState"
-      />
-    </div>
-    <div 
-      v-else
-      :style="{
-        width: '100%',
-        height: '500px',
-      }"  
-      class="d-flex justify-center align-center"
-    >
-      <div class="text-center">
-        <p class="ma-10">You are not Logged In</p>
-        <v-btn
-          @click="redirectToGoogleAuth()"
-          flat
-        >Log In</v-btn>
-      </div>
-    </div>
+      </template>
+    </IsUserLoggedInWrapper>
   </div>
 </template>
 
@@ -53,16 +39,16 @@ import { ref } from 'vue'
 import { storeToRefs } from 'pinia'
 
 import type { TimesheetStateTypes } from '../stores/useDataStore'
-import { useHandleTimesheetDisplay, useGoogleUserData } from '../stores/useDataStore'
+import { useHandleTimesheetDisplay } from '../stores/useDataStore'
 import { useLoadingScreen } from '../stores/useUserInterfaceStore'
 
 import EditTimesheet from '../components/EditTimesheet.vue'
 import TimesheetListDisplay from '../components/TimesheetListDisplay.vue'
+import IsUserLoggedInWrapper from '../components/IsUserLoggedInWrapper.vue'
 
 const useLoadingScreenStore = useLoadingScreen()
 const { isTimesheetListLoading } = storeToRefs(useLoadingScreenStore)
 const { resetTimesheetDisplay, setTimesheetDisplayStatus } = useHandleTimesheetDisplay()
-const { isUserLoggedIn, redirectToGoogleAuth } = useGoogleUserData()
 
 const state = ref<TimesheetStateTypes>('allTimesheets')
 
