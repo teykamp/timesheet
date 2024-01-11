bfec05b63ff5cb06fe053ab23f953c4e9b7e63a1
Thomas Eykamp <embergameslp@gmail.com>
2024-01-11 18:10:06 -0500

    add viewtimesheetnotes content

--- client/src/components/ViewTimesheetNote.vue
+++ client/src/components/ViewTimesheetNote.vue
@@ -4,39 +4,75 @@
       <v-expansion-panels variant="accordion">
         <v-expansion-panel
           v-for="note in timesheetNotes"
-          :key="note.timesheetId"
+          :key="note.noteid"
         >
           <v-expansion-panel-title>
-              Item
-              <template v-slot:actions="{ expanded }">
-                <v-icon 
-                  v-if="note.requireresubmit"
-                  color="warning" 
-                  icon="mdi-alert"
-                ></v-icon>
-                <v-icon :icon="expanded ? 'mdi-chevron-up' : 'mdi-chevron-down'"></v-icon>
-              </template>
+            <v-icon icon="mdi-comment-outline"></v-icon>
+            <template v-slot:actions="{ expanded }">
+              <v-icon 
+                v-if="note.requireresubmit"
+                color="warning" 
+                icon="mdi-alert"
+              ></v-icon>
+              <v-icon :icon="expanded ? 'mdi-chevron-up' : 'mdi-chevron-down'"></v-icon>
+            </template>
             </v-expansion-panel-title>
             <v-expansion-panel-text>
-              <v-card flat>
-                {{ note }}
+              <v-card flat class="py-6">
+                <v-alert
+                    v-if="note.requireresubmit"
+                    density="compact"
+                    type="warning"
+                    variant="tonal"
+                    text="Timesheet Requires Resubmission"
+                    class="mb-2"
+                  ></v-alert>
+                <v-card-title>
+                  Comments:
+                </v-card-title>
+                <v-card-text>
+                  {{ note.commentbody || "No Comments Provided" }}
+                </v-card-text>
+                <v-chip v-if="note.incorrecthours" 
+                  size="small" 
+                  color="primary" 
+                  variant="tonal" 
+                  label
+                >Hours</v-chip>
+                <v-chip v-if="note.incorrectproject" 
+                  size="small" 
+                  color="primary" 
+                  variant="tonal" 
+                  label
+                >Project</v-chip>
+                <v-chip v-if="note.incorrecttime" 
+                  size="small" 
+                  color="primary" 
+                  variant="tonal" 
+                  label
+                >Time</v-chip>
               </v-card>
             </v-expansion-panel-text>
           </v-expansion-panel>
       </v-expansion-panels>
-    <!-- list of items with pop-down with more details. can have tags with the three options: hours, project, time and a warning icon if resubmit required -->
     <div v-if="!areTimesheetNotesLoading && timesheetNotes.length === 0">
       No Timesheet Notes
     </div>
-    <div class="position-absolute bottom-8" style="left: 30%;">
+    <div class="position-absolute bottom-8 d-flex">
       <v-btn
-        v-if="timesheetDisplayStatus !== 'review'"
+        v-if="timesheetDisplayStatus !== 'review' && currentRouteName === 'admin'"
         @click="showDialog(true, CreateTimesheetNote, componentProps)"
         prepend-icon="mdi-comment-plus-outline"
-        class="mr-6"
+        class="mx-3"
       >New Comment</v-btn>
       <v-btn
+        @click="openTimesheetFromExternal(props.componentProps.timesheetId, needResubmit ? 'edit' : 'view'), closeDialog()"
+        class="mx-3"
+        prepend-icon="mdi-arrow-right"
+      >Go To Timesheet</v-btn>
+      <v-btn
         @click="closeDialog()"
+        class="mx-3"
         color="red"
       >Close</v-btn>
     </div>
@@ -48,23 +84,32 @@ import CreateTimesheetNote from './CreateTimesheetNote.vue'
 import IsContentLoadingWrapper from './IsContentLoadingWrapper.vue'
 
 import axios from 'axios'
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import { storeToRefs } from 'pinia'
+import { useRoute } from 'vue-router'
 
 import { useDialog } from '../stores/useUserInterfaceStore'
 import { useHandleTimesheetDisplay } from '../stores/useDataStore'
 import type { TimesheetNote } from '../types/types'
 
 const { showDialog, closeDialog } = useDialog()
+const { openTimesheetFromExternal } = useHandleTimesheetDisplay()
 const timesheetDisplayStatusStore = useHandleTimesheetDisplay()
 const { timesheetDisplayStatus } = storeToRefs(timesheetDisplayStatusStore)
+const route = useRoute()
+const currentRouteName = computed(() => route.name)
+
 
 const props = defineProps<{
   componentProps: { timesheetId: number }
 }>()
 
-const timesheetNotes = ref<TimesheetNote[]>([])
+const timesheetNotes = ref<(TimesheetNote & { noteid: number })[]>([])
 const areTimesheetNotesLoading = ref(false)
+
+const needResubmit = computed(() => {
+  return timesheetNotes.value.some(note => note.requireresubmit)
+})
 
 const fetchTimesheetNotes = async () => {
   areTimesheetNotesLoading.value = true
