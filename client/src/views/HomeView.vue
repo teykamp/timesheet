c71632f077adb6d5a789b921f3aee403f7e770bf
eykamt <embergameslp@gmail.com>
2024-02-22 00:42:10 -0500

    fix styling errors

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -14,10 +14,11 @@
     <v-img
       aspect-ratio="1"
       fit
+      :cover="smAndUp ? false : true"
       :style="{
         'min-width': '60%',
       }"
-      :class="{'max-width-60': mdAndDown}"
+      :class="{'max-width-60': mdAndUp}"
       src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fglobal-uploads.webflow.com%2F58868bcd2ef4daaf0f072902%2F5bebee0aa80ae83fb5ef5e47_excel-time-sheet-example.png&f=1&nofb=1&ipt=a3c9cc8ae27d32acf2269ddd6adf6915ea3d496b5c3015ebf51ec50601b72d12&ipo=images"
     ></v-img>
   
@@ -31,7 +32,7 @@ import { useDisplay } from 'vuetify'
 import { useGoogleUserData } from '../stores/useDataStore'
 
 const { getGoogleProfileDataFromGoogle } = useGoogleUserData()
-const { smAndDown, mdAndDown } = useDisplay()
+const { smAndUp, mdAndUp, xs } = useDisplay()
 
 onMounted(() => {
   getGoogleProfileDataFromGoogle()
@@ -43,8 +44,8 @@ const text = {
 
 const computeTitleStyles = computed(() => {
   return {
-    fontSize: smAndDown.value ? '2rem' : '3vw',
-    'max-width': smAndDown.value ? '100%' : '40%',
+    fontSize: mdAndUp.value ? '3vw' : '1.5rem',
+    'max-width': smAndUp.value ? '40%' : '100%',
     'font-weight': 'bold',
     'padding-left': '5%',
   } as StyleValue
